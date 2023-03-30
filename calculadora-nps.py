2b8db990f2c72fb227f0a3a0bfea22e49655dfa7
Ingrevallo <75336258+ingrevallo@users.noreply.github.com>
2023-03-30 15:58:37 -0300

    Update calculadora-nps.py

--- calculadora-nps.py
+++ calculadora-nps.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Nesta parte é solicitado ao usuário a quantidade recebida de cada nota na escala de 0 a 10
+import nps_function
+
 nota_zero = int(input('Digita a quantidade recebida de notas Zero: '))
 nota_01 = int(input('Digita a quantidade recebida de notas "1" '))
 nota_02 = int(input('Digita a quantidade recebida de notas "2" '))
@@ -13,27 +14,6 @@ nota_08 = int(input('Digita a quantidade recebida de notas "8" '))
 nota_09 = int(input('Digita a quantidade recebida de notas "9" '))
 nota_10 = int(input('Digita a quantidade recebida de notas "10" '))
 
-# Nesta parte é realizado o agrupamento das notas em Detratores, Neutros e Promotores
-detratores = nota_zero + nota_01 + nota_02 + nota_03 + nota_04 + nota_05 + nota_06
-neutros = nota_07 + nota_08
-promotores = nota_09 + nota_10
+calcular_nps = nps_function.nps(nota_zero, nota_01, nota_02, nota_03, nota_04, nota_05, nota_06, nota_07, nota_08, nota_09, nota_10)
 
-total_de_notas = detratores + neutros + promotores
-
-percentual_detratores = (detratores/total_de_notas)*100
-percentual_neutros = (neutros/total_de_notas)*100
-percentual_promotores = (promotores/total_de_notas)*100
-
-"""
-O valor do NPS é calculado da seguinte maneira:
-NPS = Percentual de Clientes Promotores (notas 9 e 10) - Percentual de Detratores (notas de 0 a 6)
-(clientes neutros não entram no cálculo)
-"""
-nps = percentual_promotores - percentual_detratores
-
-# É retornado ao usuário o valor do NPS e a quantidade de Detratores, Neutros e Promotores.
-print(str("A quantidade total de Detratores é: ") + str(detratores) + str(" - ")+ str(round(percentual_detratores,2)) + str("% das notas recebidas"))
-print(str("A quantidade total de Neutros é: ") + str(neutros) + str(" - ")+ str(round(percentual_neutros, 2)) + str("% das notas recebidas"))
-print(str("A quantidade total de Promotores é: ") + str(promotores) + str(" - ")+ str(round(percentual_promotores, 2)) + str("% das notas recebidas"))
-
-print(str("O seu NPS é: ") + str(round(nps, 2)))
+print(str("O seu NPS é: ") + str(round(calcular_nps, 2)))
